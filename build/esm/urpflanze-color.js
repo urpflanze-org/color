fabce7181433fab0405efb117693b9bc2fc8544e
genbs <gennaro.bs@gmail.com>
2021-07-26 20:36:30 +0200

    v0.0.4

---
+++ build/esm/urpflanze-color.js
@@ -0,0 +1,467 @@
+/*!
+ * @license Urpflanze Color v"0.0.4"
+ * urpflanze-color.js
+ *
+ * Github: https://github.com/urpflanze-org/color
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+/******/ "use strict";
+/******/ var __webpack_modules__ = ([
+/* 0 */,
+/* 1 */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+
+//# sourceMappingURL=types.js.map
+
+/***/ }),
+/* 2 */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "rgbToHex": () => (/* binding */ rgbToHex),
+/* harmony export */   "hslToRgb": () => (/* binding */ hslToRgb),
+/* harmony export */   "rgbToHsl": () => (/* binding */ rgbToHsl)
+/* harmony export */ });
+/**
+ * Convert rgb to hex
+ *
+ * @param r number between 0 - 255
+ * @param g number between 0 - 255
+ * @param b number between 0 - 255
+ * @returns #ffffff
+ */
+function rgbToHex(r, g, b) {
+    return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
+}
+/**
+ * Convert hsl (0-360, 0-100, 0-100) color to rgb(0-255, 0-255, 0-255)
+ *
+ * @param {number} h number between 0 - 360
+ * @param {number} s number between 0 - 100
+ * @param {number} l number between 0 - 100
+ * @returns {[number, number, number]} [0-255, 0-255, 0-255]
+ */
+function hslToRgb(h, s, l) {
+    h /= 360;
+    s /= 100;
+    l /= 100;
+    let r, g, b;
+    if (s == 0) {
+        r = g = b = l; // achromatic
+    }
+    else {
+        const hue2rgb = (p, q, t) => {
+            t += t < 0 ? 1 : t > 1 ? -1 : 0;
+            if (t < 1 / 6)
+                return p + (q - p) * 6 * t;
+            if (t < 1 / 2)
+                return q;
+            if (t < 2 / 3)
+                return p + (q - p) * (2 / 3 - t) * 6;
+            return p;
+        };
+        const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        const p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1 / 3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1 / 3);
+    }
+    return [(0.5 + r * 255) << 0, (0.5 + g * 255) << 0, (0.5 + b * 255) << 0];
+}
+/**
+ * Convert rbg (0-255, 0-255, 0-255) to hsl (0-360, 0-100, 0-100)
+ *
+ * @param {number} r number between 0 - 255
+ * @param {number} g number between 0 - 255
+ * @param {number} b number between 0 - 255
+ * @returns {[number, number, number]} (0-360, 0-100, 0-100)
+ */
+function rgbToHsl(r, g, b) {
+    r /= 255;
+    g /= 255;
+    b /= 255;
+    const max = Math.max(r, g, b);
+    const min = Math.min(r, g, b);
+    const l = (max + min) / 2;
+    let h, s;
+    if (max === min) {
+        h = s = 0;
+    }
+    else {
+        const d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch (max) {
+            case r:
+                h = (g - b) / d + (g < b ? 6 : 0);
+                break;
+            case g:
+                h = (b - r) / d + 2;
+                break;
+            case b:
+                h = (r - g) / d + 4;
+                break;
+        }
+        h = h / 6;
+    }
+    return [(0.5 + h * 360) << 0, (0.5 + s * 100) << 0, (0.5 + l * 100) << 0];
+}
+//# sourceMappingURL=conversions.js.map
+
+/***/ }),
+/* 3 */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "parseColorAndConvert": () => (/* binding */ parseColorAndConvert),
+/* harmony export */   "parseColor": () => (/* binding */ parseColor)
+/* harmony export */ });
+/* harmony import */ var _htmlcolors__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(4);
+/* harmony import */ var _conversions__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(2);
+
+
+/**
+ * Convert color to IConvertedColor
+ * Supported format: 'hsla?' 'rgba?' 'hex{3,8}' number (0xFFFFFF[FF])
+ * hsla format: hsla(360, 100%, 100%, 1)
+ * rgba format: rgb(255, 255, 255, 1)
+ *
+ * @param {(string | number)} color
+ * @returns {(IConvertedColor | undefined)}
+ */
+function parseColorAndConvert(color) {
+    const parsed = parseColor(color);
+    if (parsed) {
+        if (parsed.type === 'hsl') {
+            const [r, g, b] = (0,_conversions__WEBPACK_IMPORTED_MODULE_1__.hslToRgb)(parsed.a, parsed.b, parsed.c);
+            return {
+                r,
+                g,
+                b,
+                h: parsed.a,
+                s: parsed.b,
+                l: parsed.c,
+                alpha: parsed.alpha,
+            };
+        }
+        else {
+            const [h, s, l] = (0,_conversions__WEBPACK_IMPORTED_MODULE_1__.rgbToHsl)(parsed.a, parsed.b, parsed.c);
+            return {
+                h,
+                s,
+                l,
+                r: parsed.a,
+                g: parsed.b,
+                b: parsed.c,
+                alpha: parsed.alpha,
+            };
+        }
+    }
+}
+/**
+ * Convert color to IColor
+ * Supported format: 'hsla?' 'rgba?' 'hex{3,8}' number (0xFFFFFF[FF])
+ * hsla format: hsla(360, 100%, 100%, 1)
+ * rgba format: rgb(255, 255, 255, 1)
+ *
+ * @param {(string | number)} color
+ * @returns {(IColor | undefined)}
+ */
+function parseColor(color) {
+    if (typeof color === 'number') {
+        if (color > 0xffffff) {
+            return {
+                type: 'rgb',
+                a: (color >> 24) & 255,
+                b: (color >> 16) & 255,
+                c: (color >> 8) & 255,
+                alpha: (color & 255) / 255,
+            };
+        }
+        else {
+            return { type: 'rgb', a: (color >> 16) & 255, b: (color >> 8) & 255, c: color & 255, alpha: 1 };
+        }
+    }
+    color = color.replace(/\s/g, '');
+    if (_htmlcolors__WEBPACK_IMPORTED_MODULE_0__.default[color])
+        color = _htmlcolors__WEBPACK_IMPORTED_MODULE_0__.default[color];
+    let match = /^#([0-9a-f]{3,8})$/i.exec(color);
+    if (match) {
+        const hex = match[1];
+        if (hex.length === 3) {
+            return {
+                type: 'rgb',
+                a: parseInt(hex[0] + hex[0], 16),
+                b: parseInt(hex[1] + hex[1], 16),
+                c: parseInt(hex[2] + hex[2], 16),
+                alpha: 1,
+            };
+        }
+        else {
+            return {
+                type: 'rgb',
+                a: parseInt(hex[0] + hex[1], 16),
+                b: parseInt(hex[2] + hex[3], 16),
+                c: parseInt(hex[4] + hex[5], 16),
+                alpha: hex.length > 6 ? parseInt(hex.substring(6), 16) / 255 : 1,
+            };
+        }
+    }
+    match = /^((hsl|rgb)a?)\((\d+),(\d+)%?,(\d+)%?,?(.+)?\)$/i.exec(color);
+    if (match) {
+        const [, , type, a, b, c, alpha] = match;
+        return {
+            type: type,
+            a: +a,
+            b: +b,
+            c: +c,
+            alpha: alpha ? +alpha : 1,
+        };
+    }
+}
+//# sourceMappingURL=parsing.js.map
+
+/***/ }),
+/* 4 */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+const colors = {
+    aliceblue: '#f0f8ff',
+    antiquewhite: '#faebd7',
+    aqua: '#00ffff',
+    aquamarine: '#7fffd4',
+    azure: '#f0ffff',
+    beige: '#f5f5dc',
+    bisque: '#ffe4c4',
+    black: '#000000',
+    blanchedalmond: '#ffebcd',
+    blue: '#0000ff',
+    blueviolet: '#8a2be2',
+    brown: '#a52a2a',
+    burlywood: '#deb887',
+    cadetblue: '#5f9ea0',
+    chartreuse: '#7fff00',
+    chocolate: '#d2691e',
+    coral: '#ff7f50',
+    cornflowerblue: '#6495ed',
+    cornsilk: '#fff8dc',
+    crimson: '#dc143c',
+    cyan: '#00ffff',
+    darkblue: '#00008b',
+    darkcyan: '#008b8b',
+    darkgoldenrod: '#b8860b',
+    darkgray: '#a9a9a9',
+    darkgreen: '#006400',
+    darkkhaki: '#bdb76b',
+    darkmagenta: '#8b008b',
+    darkolivegreen: '#556b2f',
+    darkorange: '#ff8c00',
+    darkorchid: '#9932cc',
+    darkred: '#8b0000',
+    darksalmon: '#e9967a',
+    darkseagreen: '#8fbc8f',
+    darkslateblue: '#483d8b',
+    darkslategray: '#2f4f4f',
+    darkturquoise: '#00ced1',
+    darkviolet: '#9400d3',
+    deeppink: '#ff1493',
+    deepskyblue: '#00bfff',
+    dimgray: '#696969',
+    dodgerblue: '#1e90ff',
+    firebrick: '#b22222',
+    floralwhite: '#fffaf0',
+    forestgreen: '#228b22',
+    fuchsia: '#ff00ff',
+    gainsboro: '#dcdcdc',
+    ghostwhite: '#f8f8ff',
+    gold: '#ffd700',
+    goldenrod: '#daa520',
+    gray: '#808080',
+    green: '#008000',
+    greenyellow: '#adff2f',
+    honeydew: '#f0fff0',
+    hotpink: '#ff69b4',
+    indianred: '#cd5c5c',
+    indigo: '#4b0082',
+    ivory: '#fffff0',
+    khaki: '#f0e68c',
+    lavender: '#e6e6fa',
+    lavenderblush: '#fff0f5',
+    lawngreen: '#7cfc00',
+    lemonchiffon: '#fffacd',
+    lightblue: '#add8e6',
+    lightcoral: '#f08080',
+    lightcyan: '#e0ffff',
+    lightgoldenrodyellow: '#fafad2',
+    lightgrey: '#d3d3d3',
+    lightgreen: '#90ee90',
+    lightpink: '#ffb6c1',
+    lightsalmon: '#ffa07a',
+    lightseagreen: '#20b2aa',
+    lightskyblue: '#87cefa',
+    lightslategray: '#778899',
+    lightsteelblue: '#b0c4de',
+    lightyellow: '#ffffe0',
+    lime: '#00ff00',
+    limegreen: '#32cd32',
+    linen: '#faf0e6',
+    magenta: '#ff00ff',
+    maroon: '#800000',
+    mediumaquamarine: '#66cdaa',
+    mediumblue: '#0000cd',
+    mediumorchid: '#ba55d3',
+    mediumpurple: '#9370d8',
+    mediumseagreen: '#3cb371',
+    mediumslateblue: '#7b68ee',
+    mediumspringgreen: '#00fa9a',
+    mediumturquoise: '#48d1cc',
+    mediumvioletred: '#c71585',
+    midnightblue: '#191970',
+    mintcream: '#f5fffa',
+    mistyrose: '#ffe4e1',
+    moccasin: '#ffe4b5',
+    navajowhite: '#ffdead',
+    navy: '#000080',
+    oldlace: '#fdf5e6',
+    olive: '#808000',
+    olivedrab: '#6b8e23',
+    orange: '#ffa500',
+    orangered: '#ff4500',
+    orchid: '#da70d6',
+    palegoldenrod: '#eee8aa',
+    palegreen: '#98fb98',
+    paleturquoise: '#afeeee',
+    palevioletred: '#d87093',
+    papayawhip: '#ffefd5',
+    peachpuff: '#ffdab9',
+    peru: '#cd853f',
+    pink: '#ffc0cb',
+    plum: '#dda0dd',
+    powderblue: '#b0e0e6',
+    purple: '#800080',
+    rebeccapurple: '#663399',
+    red: '#ff0000',
+    rosybrown: '#bc8f8f',
+    royalblue: '#4169e1',
+    saddlebrown: '#8b4513',
+    salmon: '#fa8072',
+    sandybrown: '#f4a460',
+    seagreen: '#2e8b57',
+    seashell: '#fff5ee',
+    sienna: '#a0522d',
+    silver: '#c0c0c0',
+    skyblue: '#87ceeb',
+    slateblue: '#6a5acd',
+    slategray: '#708090',
+    snow: '#fffafa',
+    springgreen: '#00ff7f',
+    steelblue: '#4682b4',
+    tan: '#d2b48c',
+    teal: '#008080',
+    thistle: '#d8bfd8',
+    tomato: '#ff6347',
+    turquoise: '#40e0d0',
+    violet: '#ee82ee',
+    wheat: '#f5deb3',
+    white: '#ffffff',
+    whitesmoke: '#f5f5f5',
+    yellow: '#ffff00',
+    yellowgreen: '#9acd32',
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (colors);
+//# sourceMappingURL=htmlcolors.js.map
+
+/***/ })
+/******/ ]);
+/************************************************************************/
+/******/ // The module cache
+/******/ var __webpack_module_cache__ = {};
+/******/ 
+/******/ // The require function
+/******/ function __webpack_require__(moduleId) {
+/******/ 	// Check if module is in cache
+/******/ 	var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 	if (cachedModule !== undefined) {
+/******/ 		return cachedModule.exports;
+/******/ 	}
+/******/ 	// Create a new module (and put it into the cache)
+/******/ 	var module = __webpack_module_cache__[moduleId] = {
+/******/ 		// no module.id needed
+/******/ 		// no module.loaded needed
+/******/ 		exports: {}
+/******/ 	};
+/******/ 
+/******/ 	// Execute the module function
+/******/ 	__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 
+/******/ 	// Return the exports of the module
+/******/ 	return module.exports;
+/******/ }
+/******/ 
+/************************************************************************/
+/******/ /* webpack/runtime/define property getters */
+/******/ (() => {
+/******/ 	// define getter functions for harmony exports
+/******/ 	__webpack_require__.d = (exports, definition) => {
+/******/ 		for(var key in definition) {
+/******/ 			if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 				Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 			}
+/******/ 		}
+/******/ 	};
+/******/ })();
+/******/ 
+/******/ /* webpack/runtime/hasOwnProperty shorthand */
+/******/ (() => {
+/******/ 	__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ })();
+/******/ 
+/******/ /* webpack/runtime/make namespace object */
+/******/ (() => {
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = (exports) => {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/ })();
+/******/ 
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "hslToRgb": () => (/* reexport safe */ _conversions__WEBPACK_IMPORTED_MODULE_1__.hslToRgb),
+/* harmony export */   "rgbToHex": () => (/* reexport safe */ _conversions__WEBPACK_IMPORTED_MODULE_1__.rgbToHex),
+/* harmony export */   "rgbToHsl": () => (/* reexport safe */ _conversions__WEBPACK_IMPORTED_MODULE_1__.rgbToHsl),
+/* harmony export */   "parseColor": () => (/* reexport safe */ _parsing__WEBPACK_IMPORTED_MODULE_2__.parseColor),
+/* harmony export */   "parseColorAndConvert": () => (/* reexport safe */ _parsing__WEBPACK_IMPORTED_MODULE_2__.parseColorAndConvert)
+/* harmony export */ });
+/* harmony import */ var _types__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
+/* harmony import */ var _conversions__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(2);
+/* harmony import */ var _parsing__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(3);
+
+
+
+//# sourceMappingURL=index.js.map
+})();
+
+var __webpack_exports__hslToRgb = __webpack_exports__.hslToRgb;
+var __webpack_exports__parseColor = __webpack_exports__.parseColor;
+var __webpack_exports__parseColorAndConvert = __webpack_exports__.parseColorAndConvert;
+var __webpack_exports__rgbToHex = __webpack_exports__.rgbToHex;
+var __webpack_exports__rgbToHsl = __webpack_exports__.rgbToHsl;
+export { __webpack_exports__hslToRgb as hslToRgb, __webpack_exports__parseColor as parseColor, __webpack_exports__parseColorAndConvert as parseColorAndConvert, __webpack_exports__rgbToHex as rgbToHex, __webpack_exports__rgbToHsl as rgbToHsl };
+
+//# sourceMappingURL=urpflanze-color.js.map
---
+++ build/umd/urpflanze-color.js
@@ -0,0 +1,15 @@
+/*!
+ * @license Urpflanze Color v"0.0.4"
+ * urpflanze-color.js
+ *
+ * Github: https://github.com/urpflanze-org/color
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	
+/******/ 	
+/******/ })()
+;
